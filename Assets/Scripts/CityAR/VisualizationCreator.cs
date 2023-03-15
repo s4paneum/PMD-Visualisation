71dac5b6a12577ad05de391412ae3cfc6a004267
s4paneum <45764527+s4paneum@users.noreply.github.com>
2023-03-15 11:43:19 +0100

    init

---
+++ Assets/Scripts/CityAR/VisualizationCreator.cs
@@ -0,0 +1,190 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace CityAR
+{
+    public class VisualizationCreator : MonoBehaviour
+    {
+
+        public GameObject districtPrefab;
+        private DataObject _dataObject;
+        private GameObject _platform;
+        private Data _data;
+
+        private void Start()
+        {
+            _platform = GameObject.Find("Platform");
+            _data = _platform.GetComponent<Data>();
+            _dataObject = _data.ParseData();
+            BuildCity(_dataObject);
+        }
+
+        private void BuildCity(DataObject p)
+        {
+            if (p.project.files.Count > 0)
+            {
+                p.project.w = 1;
+                p.project.h = 1;
+                p.project.deepth = 1;
+                BuildDistrict(p.project, false);
+            }
+        }
+
+        /*
+         * entry: Single entry from the data set. This can be either a folder or a single file.
+         * splitHorizontal: Specifies whether the subsequent children should be split horizontally or vertically along the parent
+         */
+        private void BuildDistrict(Entry entry, bool splitHorizontal)
+        {
+            if (entry.type.Equals("File"))
+            {
+                //TODO if entry is from type File, create building
+            }
+            else
+            {
+                float x = entry.x;
+                float z = entry.z;
+
+                float dirLocs = entry.numberOfLines;
+                entry.color = GetColorForDepth(entry.deepth);
+
+                BuildDistrictBlock(entry, false);
+
+                foreach (Entry subEntry in entry.files) {
+                    subEntry.x = x;
+                    subEntry.z = z;
+                    
+                    if (subEntry.type.Equals("Dir"))
+                    {
+                        float ratio = subEntry.numberOfLines / dirLocs;
+                        subEntry.deepth = entry.deepth + 1;
+
+                        if (splitHorizontal) {
+                            subEntry.w = ratio * entry.w; // split along horizontal axis
+                            subEntry.h = entry.h;
+                            x += subEntry.w;
+                        } else {
+                            subEntry.w = entry.w;
+                            subEntry.h = ratio * entry.h; // split along vertical axis
+                            z += subEntry.h;
+                        }
+                    }
+                    else
+                    {
+                        subEntry.parentEntry = entry;
+                    }
+                    BuildDistrict(subEntry, !splitHorizontal);
+                }
+
+                if (!splitHorizontal)
+                {
+                    entry.x = x;
+                    entry.z = z;
+                    if (ContainsDirs(entry))
+                    {
+                        entry.h = 1f - z;
+                    }
+                    entry.deepth += 1;
+                    BuildDistrictBlock(entry, true);
+                }
+                else
+                {
+                    entry.x = -x;
+                    entry.z = z;
+                    if (ContainsDirs(entry))
+                    {
+                        entry.w = 1f - x;
+                    }
+                    entry.deepth += 1;
+                    BuildDistrictBlock(entry, true);
+                }
+            }
+        }
+
+        /*
+         * entry: Single entry from the data set. This can be either a folder or a single file.
+         * isBase: If true, the entry has no further subfolders. Buildings must be placed on top of the entry
+         */
+        private void BuildDistrictBlock(Entry entry, bool isBase)
+        {
+            if (entry == null)
+            {
+                return;
+            }
+            
+            float w = entry.w; // w -> x coordinate
+            float h = entry.h; // h -> z coordinate
+            
+            if (w * h > 0)
+            {
+                GameObject prefabInstance = Instantiate(districtPrefab, _platform.transform, true);
+
+                if (!isBase)
+                {
+                    prefabInstance.name = entry.name;
+                    prefabInstance.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = entry.color;
+                    prefabInstance.transform.localScale = new Vector3(entry.w, 1f,entry.h);
+                    prefabInstance.transform.localPosition = new Vector3(entry.x, entry.deepth, entry.z);
+                }
+                else
+                {
+                    prefabInstance.name = entry.name+"Base";
+                    prefabInstance.transform.GetChild(0).rotation = Quaternion.Euler(90,0,0);
+                    prefabInstance.transform.localScale = new Vector3(entry.w, 1,entry.h);
+                    prefabInstance.transform.localPosition = new Vector3(entry.x, entry.deepth+0.001f, entry.z);
+
+                }
+                
+                Vector3 scale = prefabInstance.transform.localScale;
+                float scaleX = scale.x - (entry.deepth * 0.005f);
+                float scaleZ = scale.z - (entry.deepth * 0.005f);
+                float shiftX = (scale.x - scaleX) / 2f;
+                float shiftZ = (scale.z - scaleZ) / 2f;
+                prefabInstance.transform.localScale = new Vector3(scaleX, scale.y, scaleZ);
+                Vector3 position = prefabInstance.transform.localPosition;
+                prefabInstance.transform.localPosition = new Vector3(position.x - shiftX, position.y, position.z + shiftZ);
+            }
+        }
+
+        private bool ContainsDirs(Entry entry)
+        {
+            foreach (Entry e in entry.files)
+            {
+                if (e.type.Equals("Dir"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        private Color GetColorForDepth(int depth)
+        {
+            Color color;
+            switch (depth)
+            {
+                case 1:
+                    color = new Color(179f / 255f, 209f / 255f, 255f / 255f);
+                    break;
+                case 2:
+                    color = new Color(128f / 255f, 179f / 255f, 255f / 255f);
+                    break;
+                case 3:
+                    color = new Color(77f / 255f, 148f / 255f, 255f / 255f);
+                    break;
+                case 4:
+                    color = new Color(26f / 255f, 117f / 255f, 255f / 255f);
+                    break;
+                case 5:
+                    color = new Color(0f / 255f, 92f / 255f, 230f / 255f);
+                    break;
+                default:
+                    color = new Color(0f / 255f, 71f / 255f, 179f / 255f);
+                    break;
+            }
+
+            return color;
+        }
+    }
+}
